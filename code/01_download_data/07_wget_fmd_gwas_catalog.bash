d5f366ebdf97662a74ff148d6334e7f3573dc364
Frederick J. Boehm <frederick.boehm@gmail.com>
2023-03-16 12:26:41 -0400

    added script for downloading publicly available fmd gwas statistics

---
+++ code/01_download_data/07_wget_fmd_gwas_catalog.bash
@@ -0,0 +1,6 @@
+
+
+wget http://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/GCST90026001-GCST90027000/GCST90026612/GCST90026612_buildGRCh37.tsv -P ~/research/fmd-MR/data/fmd/
+wget http://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/GCST90026001-GCST90027000/GCST90026612/md5sum.txt -P ~/research/fmd-MR/data/fmd/
+wget http://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/GCST90026001-GCST90027000/GCST90026612/README.txt -P ~/research/fmd-MR/data/fmd/
+
