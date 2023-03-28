7cf2203e7936cf2e82deab816d2ce5b635d93ad5
Frederick J. Boehm <frederick.boehm@gmail.com>
2023-03-28 13:52:49 -0400

    updated bash script

--- code/01_download_data/06_wget_barton_creatinine_2021.bash
+++ code/01_download_data/06_wget_barton_creatinine_2021.bash
@@ -3,5 +3,6 @@
 mydir=~/research/fmd-MR/barton2021_creatinine/
 mkdir -p ${mydir}
 wget http://ftp.ebi.ac.uk/pub/databases/gwas/summary_statistics/GCST90025001-GCST90026000/GCST90025946/GCST90025946_buildGRCh37.tsv -P ${mydir}
-
+# https://www.ebi.ac.uk/gwas/efotraits/EFO_0004518
+# sample size: 437660 Europeans per GWAS CATALOG URL above
 
