215a977ae35d9a2d95f72575ecda1047eacb8abb
Frederick J. Boehm <frederick.boehm@gmail.com>
2023-04-10 21:55:05 -0400

    rewrote parse_correlated_alleles function

---
+++ R/align_alleles.R
@@ -0,0 +1,20 @@
+#' Parse ldlink output for proxy SNPs' alleles in column called Correlated_Alleles
+#' 
+#' @param allele_string a character string of alleles from the Correlated_Alleles column with form like A=A,G=C
+#' @details lead SNP's alleles are always on the left side of the equals sign in the Correlated_Alleles column
+#' @return a tibble with two rows per proxy SNP
+#' @export
+#' @examples
+#' a_string <- "A=A,G=C"
+#' parse_correlated_alleles(a_string)
+parse_correlated_alleles <- function(allele_string){
+    two_with_equals <- stringr::str_split(allele_string, pattern = ",") %>%
+        unlist() 
+    result <- two_with_equals %>%
+        stringr::str_split(pattern = "=") %>%
+        unlist() %>%
+        matrix(ncol = 2, byrow = TRUE) %>%
+        tibble::as_tibble() %>%
+        dplyr::rename(lead_allele = V1, proxy_allele = V2)
+    return(result)
+}
