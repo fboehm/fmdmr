ec81e51ff1f0483049fb01c9cd1659f4c2ca836f
Frederick J. Boehm <frederick.boehm@gmail.com>
2023-04-20 16:19:57 -0400

    feat: added shell script to download mrcieu gwas summary statistics files

---
+++ analysis/code/01_download_data/wget_mrcieu.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# use neale lab's phenotype manifest file (), field CA "wget"
+# https://docs.google.com/spreadsheets/d/1AeeADtT0U1AukliiNyiVzVRdLYPkTbruQSk38DeutU8/edit#gid=268241601
+
+
+
+outdir=~/research/fmdmr/analysis/data/mrcieu/
+mkdir -p ${outdir}
+tsv_dir=~/research/fmdmr/analysis/data/mrcieu/
+url_stem=https://gwas.mrcieu.ac.uk/files
+# https://gwas.mrcieu.ac.uk/files/ieu-a-1102/ieu-a-1102.vcf.gz
+for file in ${tsv_dir}; do
+    if [[ ${file} == *.tsv ]]; then
+        while read -r line; do
+            id=$(echo ${line} | cut -f1 -d' ')
+            url=${url_stem}/${id}/${id}.vcf.gz
+            wget ${url} -P ${outdir}
+        done < ${file}
+    fi 
+done
+
+
+
+wget https://gwas.mrcieu.ac.uk/files/ieu-a-1102/ieu-a-1102.vcf.gz -P ${outdir}
