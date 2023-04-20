be36a4a69fbbef9923194fc569215a4578323c35
Frederick J. Boehm <frederick.boehm@gmail.com>
2023-04-20 16:30:54 -0400

    fix: added * to get list of all files in a directory

--- analysis/code/01_download_data/wget_mrcieu.bash
+++ analysis/code/01_download_data/wget_mrcieu.bash
@@ -7,19 +7,22 @@
 
 outdir=~/research/fmdmr/analysis/data/mrcieu/
 mkdir -p ${outdir}
-tsv_dir=~/research/fmdmr/analysis/data/mrcieu/
+tsv_dir=~/research/fmdmr/analysis/data/mrcieu
 url_stem=https://gwas.mrcieu.ac.uk/files
 # https://gwas.mrcieu.ac.uk/files/ieu-a-1102/ieu-a-1102.vcf.gz
-for file in ${tsv_dir}; do
+for file in ${tsv_dir}/*; do
     if [[ ${file} == *.tsv ]]; then
         while read -r line; do
             id=$(echo ${line} | cut -f1 -d' ')
+            echo ${id}
             url=${url_stem}/${id}/${id}.vcf.gz
-            wget ${url} -P ${outdir}
+            echo ${url}
+            if [[ ! -f ${outdir}/${id}.vcf.gz ]]; then
+                wget ${url} -P ${outdir}
+            fi
         done < ${file}
     fi 
 done
 
 
 
-wget https://gwas.mrcieu.ac.uk/files/ieu-a-1102/ieu-a-1102.vcf.gz -P ${outdir}
