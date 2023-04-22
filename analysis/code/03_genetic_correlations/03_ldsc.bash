ca5d393f13a97f2070238882236dac2223e692d4
Frederick J. Boehm <frederick.boehm@gmail.com>
2023-04-22 08:52:53 -0400

    fix: split ldsc and munge code into two files.

---
+++ analysis/code/03_genetic_correlations/03_ldsc.bash
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#SBATCH --partition=mulan,main
+#SBATCH --time=1-00:00:00
+#SBATCH --job-name=ldsc
+#SBATCH --mem=64G
+#SBATCH --cpus-per-task=1
+#SBATCH --array=1-78%20
+#SBATCH --output=/net/mulan/home/fredboe/research/fmdmr/analysis/cluster_outputs/ldsc_%a.out
+#SBATCH --error=/net/mulan/home/fredboe/research/fmdmr/analysis/cluster_outputs/ldsc_%a.err
+
+
+
+
+##### LD SCORE REGRESSION to get genetic correlations
+LDSC=~/ldsc/ldsc.py
+ldsc_genetic_corr_dir=~/research/fmdmr/analysis/data/ldsc_genetic_correlations/
+mkdir -p ${ldsc_genetic_corr_dir}
+ldsc_dir=~/research/fmdmr/analysis/data/ldsc/
+eur_dir=~/research/fmdmr/analysis/data/ldsc/eur_w_ld_chr/
+
+let k=0 # counter
+
+
+for fmd_sumstats_file in ${fmd_sumstats_dir}*.sumstats.gz; do
+    fmdstem=$(basename "$fmd_sumstats_file" .sumstats.gz)
+    echo ${fmdstem}
+    for file in ${ldsc_dir}*; do
+        if [[ ${file} == *.sumstats.gz ]]; then
+            filestem=$(basename "$file" .sumstats.gz)
+            echo ${filestem}
+            let k+=1
+            if [ ${k} -eq ${SLURM_ARRAY_TASK_ID} ]; then
+
+                conda run -n ldsc python ${LDSC} \
+                    --rg ${ldsc_dir}${file},${fmd_sumstats_file} \
+                    --ref-ld-chr ${eur_dir} \
+                    --w-ld-chr ${eur_dir} \
+                    --out ${ldsc_genetic_corr_dir}${filestem}_${fmdstem}
+            fi
+        fi 
+    done
+done
+
