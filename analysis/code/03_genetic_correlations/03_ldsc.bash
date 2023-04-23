ae7d535ec21444d35da9d319de0cbcc9626a5bae
Frederick J. Boehm <frederick.boehm@gmail.com>
2023-04-23 12:04:36 -0400

    fix: finished updates to ldsc slurm submission file

--- analysis/code/03_genetic_correlations/03_ldsc.bash
+++ analysis/code/03_genetic_correlations/03_ldsc.bash
@@ -19,6 +19,8 @@ mkdir -p ${ldsc_genetic_corr_dir}
 ldsc_dir=~/research/fmdmr/analysis/data/ldsc/
 eur_dir=~/research/fmdmr/analysis/data/ldsc/eur_w_ld_chr/
 source ~/.bashrc # prep for using conda. Is this needed?
+fmd_sumstats_dir=~/research/fmdmr/analysis/data/ldsc_fmd/
+
 
 let k=0 # counter
 
@@ -28,11 +30,11 @@ for fmd_sumstats_file in ${fmd_sumstats_dir}*.sumstats.gz; do
     for file in ${ldsc_dir}*; do
         if [[ ${file} == *.sumstats.gz ]]; then
             filestem=$(basename "$file" .sumstats.gz)
-            echo ${filestem}
+            echo "file is ${file} and fmd_sumstats_file is ${fmd_sumstats_file}\n"
             let k=${k}+1
             if [ ${k} -eq ${SLURM_ARRAY_TASK_ID} ]; then
                 conda run -n ldsc python ${LDSC} \
-                    --rg ${ldsc_dir}${file},${fmd_sumstats_file} \
+                    --rg ${file},${fmd_sumstats_file} \
                     --ref-ld-chr ${eur_dir} \
                     --w-ld-chr ${eur_dir} \
                     --out ${ldsc_genetic_corr_dir}${filestem}_${fmdstem}
